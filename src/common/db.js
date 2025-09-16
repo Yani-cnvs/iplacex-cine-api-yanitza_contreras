4d35469598c043fbe9610e3242ed6095b7237263
Yani-cnvs <yani.contreras.v@gmail.com>
2025-09-16 12:46:07 -0300

    corrección de usuario

--- src/common/db.js
+++ src/common/db.js
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const uri = 'mongodb+srv://ycontrerasv:Yani1703@eva-u3-express.7vl9boz.mongodb.net/?retryWrites=true&w=majority&appName=eva-u3-express'
+const uri = 'mongodb+srv://yanicontrerasv:Yani1703@eva-u3-express.7vl9boz.mongodb.net/?retryWrites=true&w=majority&appName=eva-u3-express'
 
 const client = new MongoClient(uri, {
     serverApi: {
